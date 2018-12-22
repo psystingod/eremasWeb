589edf29bfcb5fcb357494ae31de309be905cd9b
Diego Herrera <goherrerago@gmail.com>
2018-12-22 11:35:03 -0600

    Servicios para los datos del cliente modificados

--- pages/infoCliente.php
+++ pages/infoCliente.php
@@ -216,7 +216,7 @@
                                         <div class="card-header" role="tab" id="headingTwo1">
                                           <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx1" href="#collapseTwo1"
                                             aria-expanded="false" aria-controls="collapseTwo1">
-                                            <h5 class="mb-0">
+                                            <h5 class="mb-0 alert alert-info">
                                               TV POR CABLE <i class="fas fa-angle-down rotate-icon"></i>
                                             </h5>
                                           </a>
@@ -253,7 +253,7 @@
                                         <div class="card-header" role="tab" id="headingTwo2">
                                           <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx1" href="#collapseTwo21"
                                             aria-expanded="false" aria-controls="collapseTwo21">
-                                            <h5 class="mb-0">
+                                            <h5 class="mb-0 alert alert-success">
                                               INTERNET <i class="fas fa-angle-down rotate-icon"></i>
                                             </h5>
                                           </a>
@@ -290,7 +290,7 @@
                                         <div class="card-header" role="tab" id="headingThree31">
                                           <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx1" href="#collapseThree31"
                                             aria-expanded="false" aria-controls="collapseThree31">
-                                            <h5 class="mb-0">
+                                            <h5 class="mb-0 alert alert-warning">
                                               TELEFONIA <i class="fas fa-angle-down rotate-icon"></i>
                                             </h5>
                                           </a>
